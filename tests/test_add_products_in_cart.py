5dfebbe2ffc8a87fcfb5c7a63497d881f5fa3cac
Nguyen Khanh Duy <nguyenk@opentext.com>
2026-02-25 14:37:27 +0700

    refactor(test): refactor add products to cart test case

--- tests/test_add_products_in_cart.py
+++ tests/test_add_products_in_cart.py
@@ -1,36 +1,45 @@
 import allure
 
-from pages.guest_page import GuestPage
+from utilities.assertions import assert_true, assert_equal
 
 
-@allure.feature("Products")
-def test_add_products_in_cart(driver, config):
+@allure.feature("Cart")
+@allure.story("Add Multiple Products to Cart")
+def test_add_products_in_cart(app, config):
+    """
+    Test Case 12: Add Products in Cart
+    Ensures that multiple products can be added and verified in the cart.
+    """
     base_url = config.get("base_url")
-    guest_page = GuestPage(driver)
+    product1 = "Blue Top"
+    product2 = "Men Tshirt"
 
-    with allure.step("Navigate to url 'https://automationexercise.com'"):
-        guest_page.navigate_to(base_url)
+    with allure.step("Launch browser and navigate to home page"):
+        home_page = app.open_site(base_url)
 
-    with allure.step("Verify that home page is visible successfully"):
-        assert guest_page.is_home_page_visible()
+    with allure.step("Verify taht home page is visible successfully"):
+        assert_true(home_page.header.is_header_visible(),
+                    "Home page failed to load", home_page)
 
     with allure.step("Click 'Products' button"):
-        products_page = guest_page.navigate_to_products_page()
+        products_page = home_page.header.click_products()
 
-    with allure.step("Hover over first product and click 'Add to cart'"):
-        products_page.add_product_to_cart_by_item("Blue Top")
+    with allure.step(f"Hover over '{product1}' and click 'Add to cart'"):
+        products_page.add_product_to_cart(product1)
 
     with allure.step("Click 'Continue Shopping' button"):
-        products_page.continue_shopping_after_add()
+        products_page.continue_shopping()
 
-    with allure.step("Hover over second product and click 'Add to cart'"):
-        products_page.add_product_to_cart_by_item("Men Tshirt")
+    with allure.step(f"Hover over '{product2}' and click 'Add to cart'"):
+        products_page.add_product_to_cart(product2)
 
     with allure.step("Click 'View Cart' button"):
         cart_page = products_page.click_view_cart()
 
     with allure.step("Verify both products are added to Cart"):
-        assert cart_page.get_cart_item_count() == 2
+        actual_count = cart_page.get_cart_item_count()
+        assert_equal(actual_count, 2, "Cart item count mismatch", cart_page)
 
     with allure.step("Verify their prices, quantity and total price"):
-        assert cart_page.are_all_cart_items_price_quantity_correct()
+        assert_true(cart_page.are_all_cart_items_price_quantity_correct(),
+                    "Price, quantity or total calculation is incorrect in cart", cart_page)
